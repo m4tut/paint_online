67c6305da8be7cc0ab9de3f40eb74ed47f4ea3c3
Максим Тутуков <m.tutukov@kokoc.tech>
2022-12-11 19:59:43 +0300

    feat(master): Подготавливает стили, верстает toolbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Canvas from './components/Canvas';
+import SettingsBar from './components/SettingsBar';
+import Toolbar from './components/Toolbar';
+import './styles/app.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <SettingsBar />
+      <Toolbar />
+      <Canvas />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SettingsBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/toolbar.scss';
+
+const SettingsBar = () => {
+  return <div className='settings-bar'></div>;
+};
+
+export default SettingsBar;
