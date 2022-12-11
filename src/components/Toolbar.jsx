ff636969552dd31fdc8d3cf2976e0bc41b2b6332
Максим Тутуков <m.tutukov@kokoc.tech>
2022-12-11 22:16:36 +0300

    feat(master): Добавляет инструменты

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import '../styles/toolbar.scss'
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import '../styles/toolbar.scss';
+import Brush from '../tools/Brush';
+import Circle from '../tools/Circle';
+import Eraser from '../tools/Eraser';
+import Line from '../tools/Line';
+import Rect from '../tools/Rect';
 
 const Toolbar = () => {
-  return <div className='toolbar'>
-    <button className='toolbar__btn brush' />
-    <button className='toolbar__btn rect' />
-    <button className='toolbar__btn circle' />
-    <button className='toolbar__btn eraser' />
-    <button className='toolbar__btn line' />
-    <input style={{ marginLeft: 10}} type='color' />
-    <button className='toolbar__btn undo' />
-    <button className='toolbar__btn redo' />
-    <button className='toolbar__btn save' />
-  </div>;
+  return (
+    <div className='toolbar'>
+      <button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))} />
+      <button className='toolbar__btn rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))} />
+      <button className='toolbar__btn circle' onClick={() => toolState.setTool(new Circle(canvasState.canvas))} />
+      <button className='toolbar__btn eraser' onClick={() => toolState.setTool(new Eraser(canvasState.canvas))} />
+      <button className='toolbar__btn line' onClick={() => toolState.setTool(new Line(canvasState.canvas))} />
+      <input style={{ marginLeft: 10 }} type='color' />
+      <button className='toolbar__btn undo' onClick={() => {}} />
+      <button className='toolbar__btn redo' onClick={() => {}} />
+      <button className='toolbar__btn save' onClick={() => {}} />
+    </div>
+  );
 };
 
 export default Toolbar;
